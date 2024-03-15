f8950bf7e1e2789d719cd76c828f5cf09f9b5ff9
DroneBreaker <brownierz01@gmail.com>
2024-03-15 14:00:40 +0000

    Initialized the app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, patch, post, web::get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/pizzas")]
+async fn all_pizzas() -> impl Responder {
+    HttpResponse::Ok().body("Pizzas available.!")
+}
+
+#[post("/buypizza")]
+async fn get_pizza() -> impl Responder {
+    HttpResponse::Ok().body("Buying pizza")
+}
+
+#[patch("/pizzas/{uuid}")]
+async fn update_pizza() -> impl Responder {
+    HttpResponse::Ok().body("Updating pizza")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(all_pizzas)
+            .service(update_pizza)
+            .service(get_pizza)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
